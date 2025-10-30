006238bb3bb16e501ed62388148d9d730ebec8a0
fominfa <fomin.fa@phystech.edu>
2025-10-30 11:31:23 +0300

    Update classCar.h

--- classCar.h
+++ classCar.h
@@ -1,11 +1,13 @@
 #include "BaseclassVehicle.h"
 
 class Car : public Vehicle {
-    Car() : Vehicle(4, "Car") {}
+public:
     void drive() override {
         std::cout << "Car is driving\n";
     }
     void stop() override {
         std::cout << "Car stopped\n";
     }
+    virtual void ride_10_minutes() = 0;
+    virtual void refuel() = 0;
 };
